dfed5248ca3ce7ff62aed20624e491c989d58eb0
heartzontmove <taewan.thebigboss@mail.kmutt.ac.th>
2020-06-15 12:53:24 +0700

    PUSH 10

--- test/touchscreen/touchscreen.ino
+++ test/touchscreen/touchscreen.ino
@@ -1,58 +1,69 @@
-#include "Nextion.h"
-SoftwareSerial HMISerial(10, 11);
+#include <Adafruit_TFTLCD.h>
+#include <Adafruit_GFX.h>
+#include <TouchScreen.h>
 
-NexText t0 = NexText(0, 2, "t0");
-NexSlider h0 = NexSlider(0, 1, "h0");
-NexGauge z0  = NexGauge(0, 3, "z0");
-NexDSButton bt0 = NexDSButton(0, 5, "bt0");
+#define TS_MINX     122
+#define TS_MINY     111
+#define TS_MAXX     942
+#define TS_MAXY     890
 
+#define YP A3
+#define XM A2
+#define YM 9
+#define XP 8
 
-NexTouch *nex_listen_list[] = {&h0, &bt0, NULL};
+#define BLACK       0x0000
+#define BLUE        0x001F
+#define RED         0xF800
+#define GREEN       0x07E0
+#define CYAN        0x07FF
+#define MAGENTA     0xF81F
+#define YELLOW      0xFFE0
+#define WHITE       0xFFFF
 
-void bt0PopCallback(void *ptr)
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 364);
+
+boolean buttonEnabled = true;
+
+void setup() 
 {
-  uint32_t dual_state;
-  NexDSButton *btn = (NexDSButton *)ptr;
-  dbSerialPrintln("b0PopCallback");
-  dbSerialPrint("ptr=");
-  dbSerialPrintln((uint32_t)ptr);
-  bt0.getValue(&dual_state);
-  
-  if (dual_state)               
-  {
-    digitalWrite(13, HIGH);           // if my dual state buton is pressed
-  }
-  else
-  {
-    digitalWrite(13, LOW);            // if not
-  }
-
-}
-void h0PopCallback(void *ptr)
-{
-  int numberx = 0;
-  int gosterge = 0;
-  uint32_t number = 0;
-  char temp[10] = {0};
-  dbSerialPrintln("h0PopCallback");
-  h0.getValue(&number);
-  utoa(number, temp, 10);
-  t0.setText(temp);
-  numberx = number;
-  gosterge = map(numberx, 0, 100, 0, 180);
-  z0.setValue(gosterge);
-}
-
-void setup(void)
-{
-  pinMode(13, OUTPUT);
-  nexInit();
-  h0.attachPop(h0PopCallback);
-  bt0.attachPop(bt0PopCallback, &bt0);
-  dbSerialPrintln("setup done");
+    tft.reset();
+    uint16_t identifier = tft.readID();
+    tft.begin(identifier);
+    tft.setRotation(1);
+    tft.fillScreen(WHITE);
+    tft.drawRect(0,0,319,240,YELLOW);
+    
+    tft.setCursor(30,40);
+    tft.setTextColor(BLACK);
+    tft.setTextSize(2);
+    tft.print("TFT LCD Touch Screen");
 }
 
-void loop(void)
+void loop() 
 {
-  nexLoop(nex_listen_list);
+    TSPoint p = ts.getPoint();
+    
+    if (p.z > ts.pressureThreshhold) 
+    {    
+        p.x = map(p.x, TS_MAXX, TS_MINX, 0, 240);
+        p.y = map(p.y, TS_MAXY, TS_MINY, 0, 320);
+            
+        if(p.x>50 && p.x<260 && p.y>180 && p.y<270 && buttonEnabled)
+        {
+            buttonEnabled = false;
+            
+            pinMode(XM, OUTPUT);
+            pinMode(YP, OUTPUT);
+            
+            tft.fillScreen(WHITE);
+            tft.drawRect(0,0,319,240,YELLOW);
+            tft.setCursor(50,70);
+            tft.setTextColor(BLACK);
+            tft.setTextSize(3);
+            tft.print(" Thank you ");
+        }  
+    }
 }
