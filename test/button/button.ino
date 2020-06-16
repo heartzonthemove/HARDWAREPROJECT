7ed4111437f1b3ff6f84d0ba9e46037d457199b0
heartzontmove <taewan.thebigboss@mail.kmutt.ac.th>
2020-06-16 17:18:23 +0700

    PUSH 11

--- test/button/button.ino
+++ test/button/button.ino
@@ -1,28 +1,20 @@
 int button = 2;                 // button at pin 2
-int blue = 3;                   // blue color at pin 3
-int green = 5;                  // green color at pin 5
-int red = 6;                    // red color at pin 6
 
 void setup() 
 {
-  pinMode(button, INPUT);
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
+    Serial.begin(9600);
+    pinMode(button, INPUT);
 }
 
 void loop() 
 {
     if (digitalRead(button) == 0) 
     {
-        digitalWrite(red, 0);                       // red light power on
-        digitalWrite(green, 0);                     // green light power on
-        digitalWrite(blue, 0);                      // blue light power on
+        Serial.println("BUTTON OFF STATE");
     }
+
     else 
     {
-        digitalWrite(red, 1);                       // red light power on
-        digitalWrite(green, 1);                     // green light power on
-        digitalWrite(blue, 1);
+        Serial.println("BUTTON ON STATE");
     }
 }
--- test/potentiometer/potentiometer.ino
+++ test/potentiometer/potentiometer.ino
@@ -1,26 +1,11 @@
-int potPin = 1;                         // select the input pin for the potentiometer
-int blue = 3;                           // blue color at pin 3
-int green = 5;                          // green color at pin 5
-int red = 6;                            // red color at pin 6
-int val = 0;                            // variable to store the value coming from the potentiometer
-
 void setup() 
 {
-  pinMode(red, OUTPUT);                 // make the red light to be the output
-  pinMode(blue, OUTPUT);                // make the blue light to be the output
-  pinMode(green, OUTPUT);               // make the green light to be the output
+    Serial.begin(9600);
 }
 
 void loop() 
 {
-    val = analogRead(potPin);           // read the value from the potentiometer
-    digitalWrite(red, HIGH);            // turn the red light on
-    digitalWrite(blue, HIGH);           // turn the red light on
-    digitalWrite(green, HIGH);          // turn the red light on
-    delay(val);                         // wip-wup from the potentiometer
-    digitalWrite(red, LOW);             // turn the red light off
-    digitalWrite(blue, LOW);            // turn the red light on
-    digitalWrite(green, LOW);           // turn the red light on
-    delay(val);                         // wip-wup from the potentiometer
+    int sensorValue = analogRead(A1);
+    Serial.println(sensorValue);
 }
  
